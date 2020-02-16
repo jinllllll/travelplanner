8e0d4bed2374a9e043db6d7e31aa6be9133673ba
FuyangShan <40450679+FuyangShan@users.noreply.github.com>
2020-02-16 14:28:36 -0800

    Initialize Spring proejct with Speing Web, Spring Data JPA, Sring Security

---
+++ backend/src/test/java/TravelPlanner/TravelPlanner/TravelPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package TravelPlanner.TravelPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
