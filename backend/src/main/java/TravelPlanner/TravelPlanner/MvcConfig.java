57d413ff27a2f8263527e958328745c89716b8e8
FuyangShan <40450679+FuyangShan@users.noreply.github.com>
2020-02-16 14:40:25 -0800

    configure Spring MVC and set up view controllers to expose homeMock and helloMock

---
+++ backend/src/main/java/TravelPlanner/TravelPlanner/MvcConfig.java
@@ -0,0 +1,13 @@
+
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
