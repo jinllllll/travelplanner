f2e2363e672bcf13a4333b211f010bea28c7e7ec
FuyangShan <40450679+FuyangShan@users.noreply.github.com>
2020-02-18 16:42:41 -0800

    Half way through the PlacesService, need to add PlanRepo to save pickedup Plans

--- backend/src/main/java/TravelPlanner/TravelPlanner/Controller/PlacesController.java
+++ backend/src/main/java/TravelPlanner/TravelPlanner/Controller/PlacesController.java
@@ -1,18 +1,7 @@
 package TravelPlanner.TravelPlanner.Controller;
 
-import TravelPlanner.TravelPlanner.Entity.Place;
-import TravelPlanner.TravelPlanner.Entity.User;
-import TravelPlanner.TravelPlanner.Repository.PlacesRepository;
-import TravelPlanner.TravelPlanner.Repository.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpServerErrorException;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class PlacesController {
