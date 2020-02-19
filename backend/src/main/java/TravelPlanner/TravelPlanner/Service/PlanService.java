fdcc9468a45c4ff2d103a8273125ccace27476a5
FuyangShan <40450679+FuyangShan@users.noreply.github.com>
2020-02-18 17:14:14 -0800

    Combine the placeRepo and Plan repo together since we load places manuly

---
+++ backend/src/main/java/TravelPlanner/TravelPlanner/Service/PlanService.java
@@ -0,0 +1,51 @@
+package TravelPlanner.TravelPlanner.Service;
+
+import TravelPlanner.TravelPlanner.Entity.Place;
+import TravelPlanner.TravelPlanner.Entity.Plan;
+import TravelPlanner.TravelPlanner.Repository.PlacesRepository;
+import TravelPlanner.TravelPlanner.Repository.PlansRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlanService {
+    PlansRepository plansRepository;
+    PlacesRepository placesRepository;
+
+    @Autowired
+    public PlanService(PlansRepository planRepository, PlacesRepository placesRepository ) {
+        super();
+        this.plansRepository = planRepository;
+        this.placesRepository = placesRepository;
+    }
+
+
+    public PlanService() {
+
+    }
+
+    //listing out an user's plan list
+    public List<Plan> getPlansByUser(Integer userId) {
+        List<Plan> userPlanList = plansRepository.findPlansByUserId(userId);
+        return userPlanList;
+    }
+
+    //add Plan
+    public Plan addPlan(Plan plan) {
+        return plansRepository.save(plan);
+    }
+
+    //delete plan
+    public boolean deletePlanById(Integer planId) {
+        Plan plan = plansRepository.getOne(planId);
+        plansRepository.delete(plan);
+        return true;
+    }
+
+    //save the plans into plansRepository
+    public void setPlansRepository(PlansRepository newRepository) {
+        this.plansRepository = newRepository;
+    }
+}
